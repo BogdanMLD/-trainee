fb4acb238a4625243b825353e91eebfe1903e7e3
BogdanMLD <bogdan.mld1999@gmail.com>
2022-07-16 10:49:36 +0300

    firstCommit

---
+++ src/main/java/com/example/emplo1/Emplo1Application.java
@@ -0,0 +1,13 @@
+package com.example.emplo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Emplo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Emplo1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/emplo1/Emplo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emplo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Emplo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
